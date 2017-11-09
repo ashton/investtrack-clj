e2bb99abe86879f5e73de1b542c525773b26930d
Matheus Ashton Silva <matheusashton@gmail.com>
2017-11-08 23:46:15 -0200

    renomeando método de tradução

--- src/investtrack/command.clj
+++ src/investtrack/command.clj
@@ -1,12 +1,12 @@
 (ns investtrack.command
-    (:require [investtrack.parsing.bovespa :as parser]
-              [investtrack.database :as db]
-              [investtrack.util :as util])
-    (:gen-class))
+  (:require [investtrack.parsing.bovespa :as parser]
+            [investtrack.database :as db]
+            [investtrack.util :as util])
+  (:gen-class))
 
 (defn -main [path]
-    (->> path 
-        parser/parse-file
-        (map util/bovespa->influx)
-        vec
-        db/insert-many))
+  (->> path
+       parser/parse-file
+       (map util/bovespa->influx-share)
+       vec
+       db/insert-many))
