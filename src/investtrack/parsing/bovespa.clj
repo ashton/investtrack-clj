8c1ace5054f04840b60b113716711c9f570c7069
Matheus Ashton Silva <matheusashton@gmail.com>
2017-11-08 23:46:53 -0200

    formatting

--- src/investtrack/parsing/bovespa.clj
+++ src/investtrack/parsing/bovespa.clj
@@ -1,22 +1,22 @@
 (ns investtrack.parsing.bovespa
-  (:require 
-    [investtrack.parsing.rules.bovespa :as bovespa-rules]
-    [clojure.java.io :refer :all]))
+  (:require
+   [investtrack.parsing.rules.bovespa :as bovespa-rules]
+   [clojure.java.io :refer :all]))
 
 (defn extract-segment [text segment-rule]
-    "extract a segment from a line, following the defined rules"
-    (let [segment
-      (let [start (:start-pos segment-rule)
-            end (:end-pos segment-rule)]
-        (subs text (dec start) end))]
-      ((:formatter segment-rule) segment)))
+  "extract a segment from a line, following the defined rules"
+  (let [segment
+        (let [start (:start-pos segment-rule)
+              end (:end-pos segment-rule)]
+          (subs text (dec start) end))]
+    ((:formatter segment-rule) segment)))
 
 (defn parse-row [row]
   "parse a row into an record following the bovespa rules"
   (reduce
-      #(assoc %1 (keyword (:id %2)) (extract-segment row %2))
-      {} 
-      bovespa-rules/historical-data))
+   #(assoc %1 (keyword (:id %2)) (extract-segment row %2))
+   {}
+   bovespa-rules/historical-data))
 
 (defn parse-rows [lines]
   "parse a vector of lines into records following bovespa rules"
@@ -24,18 +24,18 @@
 
 (defn skip-first-and-last [lseq]
   (drop-last
-    (next lseq)))
+   (next lseq)))
 
 (defn open-file-lazy [path]
   (letfn [(read-next-line [rdr]
             (lazy-seq
-              (if-let [line (.readLine rdr)]
-                (cons line (read-next-line rdr))
-                (do (.close rdr) nil))))]
-         (read-next-line (clojure.java.io/reader path))))
+             (if-let [line (.readLine rdr)]
+               (cons line (read-next-line rdr))
+               (do (.close rdr) nil))))]
+    (read-next-line (clojure.java.io/reader path))))
 
 (defn parse-file [path]
   "parse every line in a file following bovespa rules, returning a vector of records"
   (parse-rows
-    (skip-first-and-last
-      (open-file-lazy path))))
+   (skip-first-and-last
+    (open-file-lazy path))))
--- src/investtrack/parsing/rules/bovespa.clj
+++ src/investtrack/parsing/rules/bovespa.clj
@@ -1,33 +1,33 @@
 (ns investtrack.parsing.rules.bovespa
-    (:require [clojure.string :refer [trim]]
-              [clj-time.format :as dt-fmt]))
+  (:require [clojure.string :refer [trim]]
+            [clj-time.format :as dt-fmt]))
 
 (defn parse-float [value]
-    "transform a 13 (last 2 decimals) digit number in a float"
-    (Float/parseFloat
-        (str
-            (subs value 0 11) "." (subs value 11))))
+  "transform a 13 (last 2 decimals) digit number in a float"
+  (Float/parseFloat
+   (str
+    (subs value 0 11) "." (subs value 11))))
 
 (defn parse-date [value]
-    "transform value to date in a yyyyMMdd Joda time format"
-    (dt-fmt/parse
-        (dt-fmt/formatter "yyyyMMdd") value))
+  "transform value to date in a yyyyMMdd Joda time format"
+  (dt-fmt/parse
+   (dt-fmt/formatter "yyyyMMdd") value))
 
 (def parse-text trim)
 
 (defn parse-num [value] (Integer/parseInt value))
 
 (def historical-data
-    [{:id "date" :start-pos 3 :end-pos 10 :formatter parse-date}
-    {:id "code" :start-pos 13 :end-pos 24 :formatter parse-text}
-    {:id "market-type" :start-pos 25 :end-pos 27 :formatter parse-text}
-    {:id "name" :start-pos 28 :end-pos 39 :formatter parse-text}
-    {:id "opening-price" :start-pos 57 :end-pos 69 :formatter parse-float}
-    {:id "max-price" :start-pos 70 :end-pos 82 :formatter parse-float}
-    {:id "min-price" :start-pos 83 :end-pos 95 :formatter parse-float}
-    {:id "avg-price" :start-pos 96 :end-pos 108 :formatter parse-float}
-    {:id "last-price" :start-pos 109 :end-pos 121 :formatter parse-float}
-    {:id "best-buy" :start-pos 122 :end-pos 134 :formatter parse-float}
-    {:id "best-sell" :start-pos 135 :end-pos 147 :formatter parse-float}
-    {:id "trade-count" :start-pos 148 :end-pos 152 :formatter parse-num}
-    {:id "trade-amount" :start-pos 153 :end-pos 170 :formatter parse-num}])
+  [{:id "date" :start-pos 3 :end-pos 10 :formatter parse-date}
+   {:id "code" :start-pos 13 :end-pos 24 :formatter parse-text}
+   {:id "market-type" :start-pos 25 :end-pos 27 :formatter parse-text}
+   {:id "name" :start-pos 28 :end-pos 39 :formatter parse-text}
+   {:id "opening-price" :start-pos 57 :end-pos 69 :formatter parse-float}
+   {:id "max-price" :start-pos 70 :end-pos 82 :formatter parse-float}
+   {:id "min-price" :start-pos 83 :end-pos 95 :formatter parse-float}
+   {:id "avg-price" :start-pos 96 :end-pos 108 :formatter parse-float}
+   {:id "last-price" :start-pos 109 :end-pos 121 :formatter parse-float}
+   {:id "best-buy" :start-pos 122 :end-pos 134 :formatter parse-float}
+   {:id "best-sell" :start-pos 135 :end-pos 147 :formatter parse-float}
+   {:id "trade-count" :start-pos 148 :end-pos 152 :formatter parse-num}
+   {:id "trade-amount" :start-pos 153 :end-pos 170 :formatter parse-num}])
