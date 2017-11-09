2aea3cd31ad2852051bcffe4cd65f8a49fe5e964
Matheus Ashton Silva <matheusashton@gmail.com>
2017-11-08 23:45:54 -0200

    traduzindo retorno do influx na query

--- src/investtrack/database.clj
+++ src/investtrack/database.clj
@@ -1,25 +1,28 @@
 (ns investtrack.database
-    (:require [capacitor.core :as influx]
-              [clojure.string :refer [join trim]]))
+  (:require [capacitor.core :as influx]
+            [clojure.string :refer [join trim]]
+            [investtrack.util :as util]))
 
 (def client (influx/make-client {:db "investtrack"}))
-(def query (partial influx/db-query client))
 (def insert-into (partial influx/write-point client))
 (def insert-many (partial influx/write-points client))
+(defn query [influx-query]
+  (let [result (influx/db-query client influx-query)]
+    (map util/influx->map (:results result))))
 
 (defn make-conditions [conditions]
-    (if (empty? conditions)
-        nil
-        (join " AND " (for [[key value] conditions] (str (name key) "=" value)))))
+  (if (empty? conditions)
+    nil
+    (join " AND " (for [[key value] conditions] (str (name key) "=" value)))))
 
 (defn select
-    "create a select statement"
-    [fields & {:keys [from where]}]
-    (let [fields-value (if (keyword? fields) "*" (join ", " fields))]
-        (let [selection (str "SELECT " fields-value)]
-            (let [source (str "FROM " from)]
-                (let [conditions 
-                    (if-let [conditions (make-conditions where)]
-                        (str "WHERE " conditions))]
-                    (trim 
-                        (join " " [selection source conditions])))))))
+  "create a select statement"
+  [fields & {:keys [from where]}]
+  (let [fields-value (if (keyword? fields) "*" (join ", " fields))]
+    (let [selection (str "SELECT " fields-value)]
+      (let [source (str "FROM " from)]
+        (let [conditions
+              (if-let [conditions (make-conditions where)]
+                (str "WHERE " conditions))]
+          (trim
+           (join " " [selection source conditions])))))))
--- src/investtrack/util.clj
+++ src/investtrack/util.clj
@@ -1,15 +1,23 @@
 (ns investtrack.util
-    (:require [clj-time.coerce :refer [to-long]]))
+  (:require [clj-time.coerce :refer [to-long]]))
 
-(defn bovespa->influx
-    "transforms a bovespa data map into a influx point"
-    [bovespa-record]
-    (def tags [:code :market-type :name])
-    (def values [:opening-price :max-price :min-price :avg-price :last-price :best-buy :best-sell :trade-count :trade-amount])
-    
-    (assoc 
-        {}
-        :measurement "shares"
-        :tags (select-keys bovespa-record tags)
-        :fields (select-keys bovespa-record values)
-        :timestamp (to-long (:date bovespa-record))))
+(defn bovespa->influx-share
+  "transforms a bovespa data map into a influx point"
+  [bovespa-record]
+  (def tags [:code :market-type :name])
+  (def values [:opening-price :max-price :min-price :avg-price :last-price :best-buy :best-sell :trade-count :trade-amount])
+
+  (assoc
+   {}
+   :measurement "shares"
+   :tags (select-keys bovespa-record tags)
+   :fields (select-keys bovespa-record values)
+   :timestamp (to-long (:date bovespa-record))))
+
+(defn influx->map
+  "tranform a influx point into a clojure map"
+  [influx-point]
+  (let [item (first (:series influx-point))]
+    (let [columns (map keyword (:columns item))]
+      (let [values (first (:values item))]
+        (zipmap columns values)))))
