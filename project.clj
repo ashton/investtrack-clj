8131f970589ce2d9c8fc76c359efb175905e7d9a
Matheus Ashton Silva <matheusashton@gmail.com>
2017-11-08 23:45:20 -0200

    adicionando dependencias no project e entrypoint standalone

--- project.clj
+++ project.clj
@@ -2,14 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :main investtrack.command
+
+  :main investtrack.main
+  :aot [investtrack.main]
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
+                 [http-kit "2.1.16"]
                  [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
                  [clj-time "0.14.0"]
                  [capacitor "0.6.0"]]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler investtrack.handler/app}
+  :ring {:handler investtrack.main/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
