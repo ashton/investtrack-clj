2a0a6fe7e46803bc0834b4f3291fa790bb4eb95b
Matheus Ashton Silva <matheusashton@gmail.com>
2017-11-11 16:01:39 -0200

    query builder test

---
+++ test/investtrack/database_test.clj
@@ -0,0 +1,20 @@
+(ns investtrack.database-test
+  (:require [clojure.test :refer :all]
+            [investtrack.database :as db]))
+
+(deftest database-test
+  (testing "query builder with :all keyword"
+    (let [query (db/find :all :in "measurement")]
+      (is (= query "SELECT * FROM measurement"))))
+      
+  (testing "query builder with specific fields"
+    (let [query (db/find ["field1" "field2"] :in "measurement")]
+      (is (= query "SELECT field1, field2 FROM measurement"))))
+    
+  (testing "query builder with :all keyword and restrictions"
+    (let [query (db/find :all :in "measurement" :where {:condition "value"})]
+      (is (= query "SELECT * FROM measurement WHERE condition=value"))))
+      
+  (testing "query builder with specific fields and multiple restrictions"
+    (let [query (db/find ["field1" "field2"] :in "measurement" :where {:condition1 "value1" :condition2 "value2"})]
+      (is (= query "SELECT field1, field2 FROM measurement WHERE condition1=value1 AND condition2=value2")))))
