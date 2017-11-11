9ef59a4f562534a1ceab99e235fc1d0bbc79e30f
Matheus Ashton Silva <matheusashton@gmail.com>
2017-11-11 00:45:53 -0200

    corrigindo modelos

--- src/investtrack/models.clj
+++ src/investtrack/models.clj
@@ -1,3 +1,8 @@
+(ns investtrack.models
+  (:require
+    [investtrack.util :refer [create-influx-point]]
+    [investtrack.database :refer [InfluxPoint]]))
+
 (defrecord Share 
   [code
    market-type
@@ -10,6 +15,24 @@
    best-buy
    best-sell
    trade-count
-   trade-amount])
+   trade-amount
+   date]
+   
+  InfluxPoint
+  (->influx-point [this]
+    (create-influx-point
+      "shares"
+      [:code :market-type :name]
+      [:opening-price :max-price :min-price :avg-price :last-price :best-buy :best-sell :trade-count :trade-amount]
+      :date
+      this)))
 
-(defrecord Transaction [date quantity share value])
+(defrecord Transaction [share market-type quantity value date]
+  InfluxPoint
+  (->influx-point [this]
+    (create-influx-point
+      "transactions"
+      [:share :market-type]
+      [:value]
+      :date
+      this)))
