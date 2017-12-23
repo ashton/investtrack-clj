b587ad2f6e36cdc52da4f57753c3e06afc403d11
Matheus Ashton Silva <matheusashton@gmail.com>
2017-12-23 15:50:22 -0200

    refatorando comando de import

--- src/investtrack/scripts/import.clj
+++ src/investtrack/scripts/import.clj
@@ -1,11 +1,20 @@
-(require '[investtrack.parsing.bovespa :as parser]
-         '[investtrack.database :as db]
-         '[investtrack.util :as util])
+(ns investtrack.scripts.import
+  (:require [clj-time.coerce :as dt]
+            [korma.core :refer [insert values]]
+            [investtrack.database.entities :as entities]
+            [investtrack.parsing.bovespa :as parser]))
 
-(do
-  (let [path (last *command-line-args*)]
-    (->> path
+(defn- parse-prices
+  [path]
+  (->> path
        parser/parse-file
-       (map util/bovespa->influx-share)
-       vec
-       db/insert-many)))
+       (map #(update % :date dt/to-timestamp))))
+
+(defn import-file
+  [& args]
+  (let [path (last args)]
+    (let [prices (parse-prices path)]
+      (let [partition-count (int (Math/ceil (/ (count prices) 1000)))]
+        (doseq [price (partition partition-count prices)]
+          (insert entities/prices
+            (values price)))))))
